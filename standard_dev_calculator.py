2d4c5dc0594028d0fdb050aae47f7bfdf2ce897a
Sabina <67659333+sinijinej@users.noreply.github.com>
2021-04-05 12:39:50 +0100

    Update standard_dev_calculator.py

--- standard_dev_calculator.py
+++ standard_dev_calculator.py
@@ -18,23 +18,22 @@ import pandas as pd
 
 def sent_std(text_string):  # data should be string
     tokenizer = RegexpTokenizer(r'\w+') 
+    
     tokenised_sent = sent_tokenize(text_string)
-
-
     x = [tokenizer.tokenize(x) for x in tokenised_sent]
-
+    
     lengthlist = []
-
+    
     for i in x:
         lengthlist.append(len(i))
-
     stdeviation = numpy.std(lengthlist)
-    
     return stdeviation
 
 def folder_sent_std(folder_path, output_path):
     file_names = os.listdir(folder_path)
+    
     file_name_and_text = {}
+    
     for file in file_names:
         with open(folder_path + file, "r") as target_file:
             file_name_and_text[file] = target_file.read()
@@ -42,7 +41,7 @@ def folder_sent_std(folder_path, output_path):
     length_list = []
     for key in file_name_and_text:
         length_list.append(sent_std(file_name_and_text[key]))
-
+        
     zip_iterator = zip(file_names, length_list)
     a_dictionary = dict(zip_iterator)
     
